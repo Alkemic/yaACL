8536a83ec8a34922d3d5b40473dc2c4f22ffa4ee
Daniel Alkemic Czuba <alkemic7@gmail.com>
2014-05-30 11:46:54 +0200

    Setting a related_name for ACL in group and user model Getting acl from user and his groups

--- yaacl/__init__.py
+++ yaacl/__init__.py
@@ -12,17 +12,17 @@ from .models import ACL
 __author__ = 'Daniel Czuba <dc@danielczuba.pl>'
 
 
-auth_and_group = (
-    getattr(settings, 'AUTH_USER_MODEL', 'auth.User').lower(),
-    getattr(settings, 'ACL_GROUP_USER_MODEL', 'auth.group').lower()
-)
+auth_and_group = {
+    getattr(settings, 'AUTH_USER_MODEL', 'auth.User').lower(): 'user',
+    getattr(settings, 'ACL_GROUP_USER_MODEL', 'auth.group').lower(): 'group'
+}
 
 
 def add_acl_field(sender, **kwargs):
     """Injection of acl m2m field into current user (auth) and group models, on `class_prepared` signal"""
 
     if sender._meta.__str__().lower() in auth_and_group:
-        field = ManyToManyField(ACL, verbose_name=_('ACL'), blank=True, null=True)
+        field = ManyToManyField(ACL, verbose_name=_('ACL'), blank=True, null=True, related_name=auth_and_group[sender._meta.__str__().lower()])
         field.contribute_to_class(sender, 'acl')
 
 class_prepared.connect(add_acl_field)
--- yaacl/functions.py
+++ yaacl/functions.py
@@ -1,7 +1,18 @@
 #-*- coding:utf-8 -*-
+from django.db.models.query_utils import Q
 from yaacl.models import ACL
 
 __author__ = 'Daniel Alkemic Czuba <dc@danielczuba.pl>'
+
+
+def get_acl_resources(user):
+    """
+    :type user: django.contrib.auth.models.User
+    :type name : str
+
+    Checks if user has rights to given resource
+    """
+    return ACL.objects.filter(Q(user=user)|Q(group__in=user.groups.all())).distinct()
 
 
 def has_access(user, resource):
@@ -17,7 +28,7 @@ def has_access(user, resource):
     if user.is_superuser:
         return True
 
-    return any([entry.resource.startswith(resource) for entry in user.acl.all()])
+    return any([entry.resource.startswith(resource) for entry in get_acl_resources(user)])
 
 
 def has_all_access(user, resource):
@@ -33,5 +44,5 @@ def has_all_access(user, resource):
     if user.is_superuser:
         return True
 
-    return set([entry.id for entry in user.acl.all() if entry.resource.startswith('news.')]) == \
+    return set([entry.id for entry in get_acl_resources(user) if entry.resource.startswith('news.')]) == \
            set([entry.id for name, entry in ACL.acl_list.items() if name.startswith('news.')])
