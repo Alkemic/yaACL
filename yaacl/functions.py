d560d743ba00d780b8e92610ab3e141796a2c9cc
Daniel Alkemic Czuba <alkemic7@gmail.com>
2014-05-30 10:58:08 +0200

    Template tag (and function) has_all_access

--- yaacl/functions.py
+++ yaacl/functions.py
@@ -1,4 +1,6 @@
 #-*- coding:utf-8 -*-
+from yaacl.models import ACL
+
 __author__ = 'Daniel Alkemic Czuba <dc@danielczuba.pl>'
 
 
@@ -9,7 +11,27 @@ def has_access(user, resource):
 
     Checks if user has rights to given resource
     """
+    if not user or not user.is_authenticated():
+        return False
+
     if user.is_superuser:
         return True
 
     return any([entry.resource.startswith(resource) for entry in user.acl.all()])
+
+
+def has_all_access(user, resource):
+    """
+    :type user: django.contrib.auth.models.User
+    :type name : str
+
+    Checks if user has rights to given resource
+    """
+    if not user or not user.is_authenticated():
+        return False
+
+    if user.is_superuser:
+        return True
+
+    return set([entry.id for entry in user.acl.all() if entry.resource.startswith('news.')]) == \
+           set([entry.id for name, entry in ACL.acl_list.items() if name.startswith('news.')])
--- yaacl/templatetags/acl.py
+++ yaacl/templatetags/acl.py
@@ -14,3 +14,14 @@ def has_access(user, resource):
     Checks if user has rights to given resource
     """
     return functions.has_access(user, resource)
+
+
+@register.filter
+def has_all_access(user, resource):
+    """
+    :type user: django.contrib.auth.models.User
+    :type name : str
+
+    Checks if user has rights to given resource
+    """
+    return functions.has_all_access(user, resource)
