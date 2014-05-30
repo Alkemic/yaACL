0c0eb4a2b6923bfcd4c4dc1b3f628d3e54d5feeb
Daniel Alkemic Czuba <alkemic7@gmail.com>
2014-05-30 11:53:16 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ f.close()
 
 setup(
     name='yaacl',
-    version='0.1.1',
+    version='0.2',
     description='Yet another access control list (ACL) per view for Django',
     long_description=readme,
     author="Daniel Czuba",
