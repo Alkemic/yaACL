6b5e1d9b35b637af9aca3d5875cddbabcab72c62
Daniel Alkemic Czuba <alkemic7@gmail.com>
2015-05-31 16:20:54 +0200

    PEP8 in migrations

--- yaacl/migrations/0001_initial.py
+++ yaacl/migrations/0001_initial.py
@@ -14,11 +14,31 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='ACL',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('resource', models.CharField(max_length=255, verbose_name='Resource name', db_index=True)),
-                ('display', models.CharField(max_length=255, null=True, verbose_name='displayed name', blank=True)),
-                ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='Creation time')),
-                ('is_available', models.BooleanField(default=True, verbose_name='Is available to assign')),
+                ('id', models.AutoField(
+                    verbose_name='ID',
+                    serialize=False,
+                    auto_created=True,
+                    primary_key=True,
+                )),
+                ('resource', models.CharField(
+                    max_length=255,
+                    verbose_name='Resource name',
+                    db_index=True,
+                )),
+                ('display', models.CharField(
+                    max_length=255,
+                    null=True,
+                    verbose_name='displayed name',
+                    blank=True,
+                )),
+                ('created_at', models.DateTimeField(
+                    auto_now_add=True,
+                    verbose_name='Creation time',
+                )),
+                ('is_available', models.BooleanField(
+                    default=True,
+                    verbose_name='Is available to assign',
+                )),
             ],
             options={
             },
