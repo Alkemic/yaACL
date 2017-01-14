724122a568226dc09fa4b073ad24904c82e803b6
Daniel Alkemic Czuba <alkemic7@gmail.com>
2017-01-14 15:41:58 +0100

    Remove hacks/monkeypatching from code

--- yaacl/admin.py
+++ yaacl/admin.py
@@ -1,14 +1,43 @@
 # -*- coding:utf-8 -*-
-from django.db.models import get_model
+from django import forms
+try:
+    from django.apps import apps
+    get_model = apps.get_model
+except ImportError:
+    from django.db.models.loading import get_model
 from django.conf import settings
 from django.contrib import admin
-from django.contrib.admin.sites import NotRegistered
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin, GroupAdmin
 from django.contrib.auth.models import Group
 from django.utils.translation import gettext_lazy as _
 
 from .models import ACL
+
+
+class ACLAdditionalForm(forms.ModelForm):
+    acl = forms.ModelMultipleChoiceField(
+        ACL.objects.all(),
+        widget=admin.widgets.FilteredSelectMultiple('ACL', False),
+        required=False,
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(ACLAdditionalForm, self).__init__(*args, **kwargs)
+        if self.instance.pk:
+            self.initial['acl'] = self.instance.acl.values_list(
+                'pk', flat=True,
+            )
+
+    def save(self, *args, **kwargs):
+        instance = super(ACLAdditionalForm, self).save(*args, **kwargs)
+
+        if not instance.pk:
+            instance.save()
+
+        instance.acl = self.cleaned_data['acl']
+
+        return instance
 
 User = get_user_model()
 
@@ -19,11 +48,11 @@ except admin.sites.NotRegistered:
 
 
 class ACLUserAdmin(UserAdmin):
+    form = ACLAdditionalForm
     fieldsets = UserAdmin.fieldsets + (
         (_('ACL'), {'fields': ('acl',)}),
     )
 
-    filter_horizontal = UserAdmin.filter_horizontal + ('acl',)
 
 if getattr(settings, 'ACL_GROUP_USER_MODEL', 'auth.Group') == 'auth.Group':
     try:
@@ -39,7 +68,7 @@ if getattr(settings, 'ACL_GROUP_USER_MODEL', 'auth.Group') == 'auth.Group':
     group_model = get_model(app_label, class_name)
 
     class ACLGroupAdmin(GroupAdmin):
-        filter_horizontal = GroupAdmin.filter_horizontal + ('acl',)
+        form = ACLAdditionalForm
 
     admin.site.register(Group, ACLGroupAdmin)
 
--- yaacl/models.py
+++ yaacl/models.py
@@ -5,10 +5,17 @@ from django.utils.encoding import python_2_unicode_compatible
 
 from .managers import ACLManager
 
+from django.contrib.auth.models import User
+from django.conf import settings
+
+user_model = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
+group_model = getattr(settings, 'ACL_GROUP_USER_MODEL', 'auth.Group')
+
 
 @python_2_unicode_compatible
 class ACL(models.Model):
     acl_list = {}
+
     resource = models.CharField(
         _("Resource name"),
         max_length=255,
@@ -27,6 +34,19 @@ class ACL(models.Model):
     is_available = models.BooleanField(
         _("Is available to assign"),
         default=True,
+    )
+
+    user = models.ManyToManyField(
+        User,
+        verbose_name=_('User'),
+        blank=True,
+        related_name='acl',
+    )
+    group = models.ManyToManyField(
+        group_model,
+        verbose_name=_('User'),
+        blank=True,
+        related_name='acl',
     )
 
     objects = ACLManager()
