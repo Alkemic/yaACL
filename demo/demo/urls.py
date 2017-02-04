d5b0cc9a2ccf55262616d889b2b792b56f6a2963
Daniel Alkemic Czuba <alkemic7@gmail.com>
2017-02-04 09:52:01 +0100

    Urls in demo are comaptible with Django 1.9+

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -1,11 +1,10 @@
 # -*- coding:utf-8 -*-
-from django.conf.urls import patterns, include, url
-
+from django.conf.urls import include, url
 from django.contrib import admin
+
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
+urlpatterns =[
     url(r'^', include('test_acl.urls', namespace='test-acl')),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
--- demo/test_acl/urls.py
+++ demo/test_acl/urls.py
@@ -1,13 +1,12 @@
 # -*- coding:utf-8 -*-
 from django.conf.urls import *
 
-from .views import Index, TestClassBasedView
+from .views import Index, TestClassBasedView, test, other_index, utf_test
 
-urlpatterns = patterns(
-    'test_acl.views',
+urlpatterns = [
     url(r'^$', Index.as_view(), name='index'),
     url(r'^$', TestClassBasedView.as_view(), name='index'),
-    url(r'test/$', 'test', name='test'),
-    url(r'other_index/$', 'other_index', name='other_index'),
-    url(r'utf_test/$', 'utf_test', name='utf_test'),
-)
+    url(r'test/$', test, name='test'),
+    url(r'other_index/$', other_index, name='other_index'),
+    url(r'utf_test/$', utf_test, name='utf_test'),
+]
